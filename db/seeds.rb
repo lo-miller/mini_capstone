cdd0e301e8e9edb1cc17f89c43945c58b5d63390
Lauren Miller <lomiller0913@gmail>
2021-02-23 20:23:13 -0500

    Seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: "tent", price: 120, image_url: "tent@rei.com", description: "2 person backpacking tent")
+
+Product.create(name: "boots", price: 85, image_url: "boots@rei.com", description: "brown hiking boots")
+
+Product.create(name: "hiking poles", price: 90, image_url: "poles@rei.com", description: "pair of black collapsible hiking poles")
+
+Product.create(name: "pack", price: 180, image_url: "pack@rei.com", description: "50L blue backpack with rainshield")
+
+Product.create(name: "hammock", price: 45, image_url: "hammock@rei.com", description: "hammock with straps, red")
