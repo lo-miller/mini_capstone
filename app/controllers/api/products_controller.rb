97bc85de7e1f8c06be597435de266d3039bfc4c0
Lauren Miller <lomiller0913@gmail>
2021-02-25 21:14:48 -0500

    add show route

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,30 +1,14 @@
 class Api::ProductsController < ApplicationController
 
   def index
-    #display list of all products
+    @index = Product.all
     render "index.json.jb"
   end
 
-  # def inventory
-  #   @inventory = Product.all
-  #   render "inventory.json.jb"
-  # end
-
-  # def boots
-  #   @product = Product.find_by({name: "boots"})
-  #   render "boots.json.jb"
-  #  end
-
-  # def tent    
-  #   @product = Product.find_by({name: "tent"})
-  #   render "tent.json.jb"
-  # end
-
-  # def query
-  #   @product = Product.find_by({name: params[:name]})
-  #   render "query_param.json.jb"
-  # end
-
+  def show
+    @product = Product.find_by(id: params[:id])
+    render "show.json.jb"
+  end
  
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,9 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
-
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
 
   namespace :api do
     get "/products" => "products#index"
-    # get "/boots" => "products#boots"
-    # get "/tent" => "products#tent"
-    # get "/query" => "products#query"
-    # get "/segment/:name" => "products#query"
+    get "/products/:id" => "products#show"
+   # post "/products" => "products#create"
   end
 
 end
