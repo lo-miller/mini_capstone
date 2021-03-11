f083435b6adcd3e1a8c815f7eaddbe8c4cf1d5e9
Lauren Miller <lomiller0913@gmail>
2021-03-10 20:02:44 -0500

    make orders dynamic

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,16 +1,29 @@
 class Api::OrdersController < ApplicationController
 
   def create
+    p "current_user"
+    p current_user
+
+    product = Product.find_by(id: params[:product_id])
+    calculated_subtotal = params[:quantity].to_i * product.price
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_subtotal + calculated_tax
+    
     @order = Order.new(
       # user_id - auth
-      # product_id - params - add validation that product exists/fetch
+      user_id: current_user.id,
+      # product_id - params - add validation that product exists
       product_id: params[:product_id],
       # quantity - params
       quantity: params[:quantity],
       # subtotal - model method
-      # total - model method
+      subtotal: calculated_subtotal,
       # tax - model method
+      tax: calculated_tax,
+      # total - model method
+      total: calculated_total
     )
+    @order.save
     render "show.json.jb"
   end
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,11 @@
 class Order < ApplicationRecord
   belongs_to :product
-  belongs_to :order
+  belongs_to :user
 
-  def subtotal
-    product = Product.find_by
-    return quantity * price
-  end
+  # def subtotal
+  #   product = Product.find_by
+  #   return quantity * price
+  # end
 
 
 
